df31fd70133f600c74c187611be4c6d2f26b190d
Brandon Tedesco <btedzz@gmail.com>
2025-12-17 15:15:20 -0500

    ios/android native code added via expo prebuild

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,13 @@
 import { registerRootComponent } from 'expo';
+import * as SplashScreen from 'expo-splash-screen';
 import App from './App.jsx';
+
+// Keep the splash screen visible while we fetch resources
+SplashScreen.preventAutoHideAsync();
+
+// Hide splash screen after app mounts
+setTimeout(() => {
+  SplashScreen.hideAsync();
+}, 500);
 
 registerRootComponent(App);
